4d27e5e064ff8d96281b297445ab70d424f91a82
pritesh-peter <pritesh.peter@gmail.com>
2022-09-08 00:00:43 +0545

    WIP

--- src/main/java/com/trn/core/api/entities/User.java
+++ src/main/java/com/trn/core/api/entities/User.java
@@ -1,7 +1,14 @@
 package com.trn.core.api.entities;
 
-import javax.persistence.Entity;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name="users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
 }
