11acb26bc560b54afa7d244b8a2d70429633e717
pritesh-peter <pritesh.peter@gmail.com>
2022-09-07 23:30:08 +0545

    created required packages

---
+++ src/test/java/com/trn/core/api/TrnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trn.core.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
