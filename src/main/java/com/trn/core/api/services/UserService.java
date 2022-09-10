ff6b75e793ff9f5aa969c23439787cf8be839afd
pritesh-peter <pritesh.peter@gmail.com>
2022-09-10 21:25:18 +0545

    users payload and services

---
+++ src/main/java/com/trn/core/api/services/UserService.java
@@ -0,0 +1,19 @@
+package com.trn.core.api.services;
+
+import com.trn.core.api.payloads.UserDto;
+
+import java.util.List;
+
+
+public interface UserService {
+
+    UserDto createUser(UserDto user);
+
+    UserDto updateUser(UserDto user, Integer userId);
+
+    UserDto getUserById(Integer userId);
+
+    List<UserDto> getAllUsers();
+
+    void deleteUser(Integer userId);
+}
---
+++ src/main/java/com/trn/core/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.trn.core.api.services.impl;
+
+import com.trn.core.api.payloads.UserDto;
+import com.trn.core.api.services.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserDto user) {
+        return null;
+    }
+
+    @Override
+    public UserDto updateUser(UserDto user, Integer userId) {
+        return null;
+    }
+
+    @Override
+    public UserDto getUserById(Integer userId) {
+        return null;
+    }
+
+    @Override
+    public List<UserDto> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void deleteUser(Integer userId) {
+
+    }
+}
