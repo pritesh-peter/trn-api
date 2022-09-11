41452330559fa592c3bf54b00ae84bf5115a4369
pritesh-peter <pritesh.peter@gmail.com>
2022-09-11 17:42:42 +0545

    User Controller update, delete

--- src/main/java/com/trn/core/api/controllers/UserController.java
+++ src/main/java/com/trn/core/api/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.trn.core.api.controllers;
 
 
+import com.trn.core.api.payloads.ApiResponse;
 import com.trn.core.api.payloads.UserDto;
 import com.trn.core.api.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,25 @@ public class UserController {
         UserDto createdUserDto =  this.userService.createUser(userDto);
         return new ResponseEntity<>(createdUserDto, HttpStatus.CREATED);
     }
+
+    //PUT- update user
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uId )
+    {
+        UserDto updatedUser = this.userService.updateUser(userDto,uId);
+        return ResponseEntity.ok(updatedUser);
+
+    }
+
+    //DELETE- delete user
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Integer uId)
+    {
+     this.userService.deleteUser(uId);
+     return new ResponseEntity<ApiResponse>(new ApiResponse("User Deleted Successfully",true),HttpStatus.OK)
+    }
+
+    //GET - get user
+
 }
