a8f49f5405607aca737f86b0d2f20a338bd78fd7
pritesh-peter <pritesh.peter@gmail.com>
2022-10-10 21:14:52 +0545

    password encoder

--- src/main/java/com/trn/core/api/config/SecurityConfig.java
+++ src/main/java/com/trn/core/api/config/SecurityConfig.java
@@ -2,11 +2,14 @@ package com.trn.core.api.config;
 
 import com.trn.core.api.security.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -30,6 +33,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(customUserDetailService).passwordEncoder(null);
+        auth.userDetailsService(customUserDetailService).passwordEncoder(passwordEncoder());
     }
+
+@Bean
+public PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+}
+
 }
--- src/main/java/com/trn/core/api/entities/User.java
+++ src/main/java/com/trn/core/api/entities/User.java
@@ -29,7 +29,7 @@ public class User implements UserDetails {
 
     private String email;
 
-    @Column(nullable = false,length = 50)
+    @Column(nullable = false,length = 200)
     private String password;
 
     private String about;
